0a35db3d622b3e5a8a92626cc1e2ead3f67630d9
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-07-08 17:24:16 +0900

    Add files via upload

---
+++ level-7_친구찾기_임서규_답안/level-7_친구찾기_임서규_답안.java
@@ -0,0 +1,7 @@
+public static void getFriend(String[] input) {
+    for(int i=0; i<input.length; i++) {
+      if(input[i].length() == 4) {
+        System.out.print(input[i]+" ");
+      }
+    }
+  }
