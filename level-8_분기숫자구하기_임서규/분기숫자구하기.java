37533b67464a055b6be1eed44f3b66961579858b
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-07-09 14:43:50 +0900

    Update 분기숫자구하기.java

--- level-8_분기숫자구하기_임서규/분기숫자구하기.java
+++ level-8_분기숫자구하기_임서규/분기숫자구하기.java
@@ -1,15 +1,22 @@
+//--------방법1------------------
 public static int input(int n) {
 	if(n==1||n==2||n==3) {
-		System.out.println(n+" => "+"1");
-	}
-	if(n==4||n==5||n==6) {
-		System.out.println(n+" => "+"2");
-	}
-	if(n==7||n==8||n==9) {
-		System.out.println(n+" => "+"3");
-	}
-	if(n==10||n==11||n==12) {
-		System.out.println(n+" => "+"4");
+		return 1;
+	}else if(n==4||n==5||n==6) {
+		return 2;
+	}else if(n==7||n==8||n==9) {
+		return 3;
+	}else if(n==10||n==11||n==12) {
+		return 4;
 	}
 	return n;
+}
+
+//--------방법2------------------
+public static int input(int n) {
+    int boongi = 0;
+    if (1<=n && n<=12){
+        boongi = (month+2)/3;
+    }
+    return boongi;
 }
