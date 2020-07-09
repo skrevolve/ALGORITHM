2d72b91ab08355c5927405424632da57af73e261
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-07-09 14:54:37 +0900

    Update 분기숫자구하기.java

--- level-8_분기숫자구하기_임서규/분기숫자구하기.java
+++ level-8_분기숫자구하기_임서규/분기숫자구하기.java
@@ -1,4 +1,4 @@
-//--------방법1------------------
+//--------방법1------------------약간 실무적인 느낌이지만 input이 1이 더많다면 이코드가 연산량이 더적다.
 public static int input(int n) {
 	if(n==1||n==2||n==3) {
 		return 1;
@@ -12,7 +12,7 @@ public static int input(int n) {
 	return n;
 }
 
-//--------방법2------------------
+//--------방법2------------------대학에서는 이런방식으로 많이 가르친다고 한다.
 public static int input(int n) {
     int boongi = 0;
     if (1<=n && n<=12){
