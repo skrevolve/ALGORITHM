81111db0bf3740aba2f49b47e3eae6033760d13a
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-07-09 19:05:14 +0900

    Update level-7_친구찾기_임서규_답안.java

--- level-7_친구찾기_임서규_답안/level-7_친구찾기_임서규_답안.java
+++ level-7_친구찾기_임서규_답안/level-7_친구찾기_임서규_답안.java
@@ -1,7 +1,9 @@
-public static void getFriend(String[] input) {
+public static ArrayList<String> getFriend(String[] input) {
+    ArrayList<String> list = new ArrayList<String>();
     for(int i=0; i<input.length; i++) {
       if(input[i].length() == 4) {
-        System.out.print(input[i]+" ");
+        list.add(input[i]); //list에 새로 담아서 return
       }
     }
-  }
+    return list;
+}
