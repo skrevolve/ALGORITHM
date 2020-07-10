aeeee5554436ae5ec6ebbdbd28ae08299c24990e
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-07-10 14:35:25 +0900

    Update level-8_사용하지않는가장작은숫자찾기_임서규_답안.java

--- level-8_사용하지_않는_가장작은_숫자_찾기_임서규/level-8_사용하지않는가장작은숫자찾기_임서규_답안.java
+++ level-8_사용하지_않는_가장작은_숫자_찾기_임서규/level-8_사용하지않는가장작은숫자찾기_임서규_답안.java
@@ -1,16 +1,16 @@
-static public int smallestInt(int[] A) {
-    Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+public static int smallestInt(int[] A) {
+    HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
     int max = 0;
-    int isFalse = -1; // 음수가 입력될경우 -1을 반환
-    
+    int isFalse = -1;
+
     for(int a : A) {
-        if(a > 0) {
+        if(a >= 0 ) { //여기서 조건을 실수해서 hashmap에서 index갯수가 하나모잘랏음 (
             map.put(a,true);
             if(max < a) {
                 max = a;
             }
-        }else if(a < 0){
-            return isFalse;
+        }else if(a < 0) {
+            return isFalse; //음수로 입력받을경우 -1 반환
         }
     }
 
@@ -18,9 +18,13 @@ static public int smallestInt(int[] A) {
         if(max == i && map.containsKey(i)) {
             return max+1;
         }
-        if(!map.containsKey(i)){
+        if(!map.containsKey(0)) { //0이 없으면 0반환
+            return 0;
+        }
+        if(!map.containsKey(i)){ //없는값 반환
             return i;
         }
     }
+
     return max;
 }
