59de5a5789c7d806817b69d5a2a2af75c15c703c
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-07-13 15:41:05 +0900

    Update level-7_중간_숫자_찾기_임서규_답안.java

--- level-7_중간_숫자_찾기_임서규_답안/level-7_중간_숫자_찾기_임서규_답안.java
+++ level-7_중간_숫자_찾기_임서규_답안/level-7_중간_숫자_찾기_임서규_답안.java
@@ -1,9 +1,23 @@
-public static int getNumber(int[] input) {
-    int isError = -1; //조건 불충족시 -1반환
-	Arrays.sort(input); //오름차순 정렬
-	if(input.length > 3){
-	    return isFalse;
-	}else{
-	    return input[1];
+//System.out.print(getNumber(new double[]{5,1,2}));
+public static int getNumber(double[] input) {
+    int max = 0;
+    int min = 0;
+    int middle = 0;
+    int maxIndex = 0;
+    int minIndex = 0;
+    int middleIndex = 0;
+
+	for(int i = 0; i<input.length; i++) {
+		if(input[i] > max) {
+			max = (int) input[i];
+			maxIndex = i;
+		}else if(input[i] < min) {
+			min = (int) input[i];
+			minIndex = i;
+		}else if(input[i] > min && input[i] < max) {
+			middle = (int) input[i];
+			middleIndex = i;
+		}
 	}
+	    return middleIndex;	
  }
