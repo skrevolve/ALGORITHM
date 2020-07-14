75059eeada9fa81054e236e9dee9867481cb1387
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-07-14 10:21:35 +0900

    Update level-8_숫자더하기_임서규.java

--- level-8_숫자더하기_임서규/level-8_숫자더하기_임서규.java
+++ level-8_숫자더하기_임서규/level-8_숫자더하기_임서규.java
@@ -1,11 +1,14 @@
 public static int input(int n) {
 	int sum = 0;
+	int isError = -1;
+	
 	if(n>=1) {
 		for(int i=0; i<=n; i++) {
 			sum += i;
 		}	
 	}else if(n<1) {
-		//error
+		return isError;
 	}
+	
 	return sum;
 }
