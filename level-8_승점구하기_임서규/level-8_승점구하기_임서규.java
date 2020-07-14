f8332234693c19d16496a2f1d6792dcd0ac51d92
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-07-14 10:20:37 +0900

    Update level-8_승점구하기_임서규.java

--- level-8_승점구하기_임서규/level-8_승점구하기_임서규.java
+++ level-8_승점구하기_임서규/level-8_승점구하기_임서규.java
@@ -8,27 +8,30 @@ public static void main(String[] args) throws IOException {
 }
 		
 public static int sum(String[] input) {	
+	
 	int total = 0;
+	
        for(int i=0; i<input.length; i++) {
-       	            int index = input[i].indexOf(":");
-		int a = Integer.parseInt(input[i].substring(0,index));
- 		int b = Integer.parseInt(input[i].substring(index+1));
-        
-       		if(a>4 && b>4) {
-			System.out.print("0~4점만 입력가능합니다\n");
-			break;
-		}
-	        
-		if(a==b) {
-			total += 1;
-		}
-		else if(a>b) {
-			total += 3;
-		}
-		else if(a<b) {
-			total += 0;
-			}				
+	int index = input[i].indexOf(":");
+	int a = Integer.parseInt(input[i].substring(0,index));
+	int b = Integer.parseInt(input[i].substring(index+1));
+
+	if(a>4 && b>4) {
+		System.out.print("0~4점만 입력가능합니다\n");
+		break;
+	}
+
+	if(a==b) {
+		total += 1;
+	}
+	else if(a>b) {
+		total += 3;
+	}
+	else if(a<b) {
+		total += 0;
+		}				
        }
+	
 	System.out.print(total);
 	return total;
 }
